e99d1c80206f0055bc9f58dfe00e772be8d09ff8
Abidh Sulaiman <abidhabu34@gmail.com>
2024-12-21 17:45:58 +0530

    Add Documentation

--- user_profile/models.py
+++ user_profile/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class CustomUser(models.Model):
+    """
+    CustomUser model represents a user in the application.
+    
+    Fields:
+    - name: Full name of the user.
+    - email: Email address of the user, which must be unique.
+    - age: Age of the user. Must be between 0 and 120.
+    """
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     age = models.PositiveIntegerField()
--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -2,17 +2,27 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the User model.
     
+    Fields: 'id','name', 'email', 'age'
+    """
     class Meta:
         model = CustomUser
         fields = ['id','name', 'email', 'age']
 
     def validate_name(self, value):
+        """
+        Validates that the name should be a non-empty string
+        """
         if len(value) == 0:
             raise serializers.ValidationError("Name cannot be empty.")
         return value
     
     def validate_age(self, value):
+        """
+        Validates that the age is between 0 and 120.
+        """
         if not (0 < value <= 120):
             raise serializers.ValidationError("Age must be between 0 and 120.")
         return value
